d1a97d1147ac86ae3571339222c8091a6ac09dc4
gdemmis1 <Gedion.Demmissie1@T-Mobile.com>
2017-03-07 12:49:46 -0800

    added data access layer

---
+++ CsvUploaderDataAccess/CsvDataContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace CsvUploaderDataAccess
+{
+    public class CsvDataContext: DbContext
+    {
+        public CsvDataContext()
+            : base("name=CvscontentsDb")
+        {
+        }
+        public DbSet<CsvFile> CsvFileContents { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+        }
+    }
+}
---
+++ CsvUploaderDataAccess/CsvFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvUploaderDataAccess
+{
+    public class CsvFile
+    {
+        public int Id { get; set; }
+        public byte[] Content { get; set; }
+        public string FileName { get; set; }
+
+    }
+}

e62078cdd59f2dcecc658059217685689ac78668
gdemmis1 <Gedion.Demmissie1@T-Mobile.com>
2017-03-07 12:43:07 -0800

    added ui layer

---
+++ CsvUploaderApp/Models/CsvFileContent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using CsvUploaderBusinessLayer.Models;
+
+namespace CsvUploaderApp.Models
+{
+    public class CsvFileContentModel
+    {
+        //  public List<CsvEntry> CsvEntries { get; set; }
+        //  public Dictionary<string,HashSet<WeightedChildNode>> ParentsWithChildrenAdjecencyList { get; set; }
+        public int Id { get; set; }
+        public string FileName { get; set; }
+    }
+}
